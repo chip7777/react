1dc7471ab8517b49fd3793b7fd2152a40a8c46fe
chip7777 <chip777@yandex.ru>
2022-04-13 00:14:09 +0300

    stage-1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React, {Component} from 'react';
-import './App.css';
-import {Message} from './components/Message';
+import { getValue } from "@testing-library/user-event/dist/utils";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-export class App extends Component {
-  render (){
-    return <h1><Message text='text'/></h1>
-  }
-} 
+export const App = () => {
+  const [messageList, SetMessageList] = useState([]);
+  const [message, SetMessage] = useState("");
+  const Author = { Men: "men", Bot: "bot" };
+  const handleInput = (ev) => SetMessage(ev.target.value);
 
+  const handleClick = (ev) => {
+    SetMessageList([...messageList, { Mess: message, Author: Author.Men }]);
+    SetMessage("");
+  };
+
+  const sendMess = () => {
+    SetMessageList([...messageList, { Mess: "Hello", Author: Author.Bot }]);
+  };
+
+
+  useEffect(() => {
+    if (messageList.length)
+      if (messageList[messageList.length - 1].Author === Author.Men) 
+        setTimeout(() => sendMess(), 1500); 
+  }, [messageList]);
+
+  return (
+    <>
+      <ul>
+        {messageList.map((el) => (
+          <li>{el.Author + " : " + el.Mess}</li>
+        ))}
+      </ul>
+      <input type="text" onInput={handleInput} value={message}></input>
+      <button onClick={handleClick}>send</button>
+    </>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './App';
-import {Message} from './components/Message';
+
 
 
 ReactDOM.render(
-  <App text='text from props' />,
+  <App />,
   document.getElementById('root')
 );
 
