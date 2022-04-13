22bade16365d1e2bf46d8a60c51b670bfa719e94
chip7777 <chip777@yandex.ru>
2022-04-13 23:51:08 +0300

    lesson-2

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
-import { getValue } from "@testing-library/user-event/dist/utils";
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import './App.css';
 
 export const App = () => {
   const [messageList, SetMessageList] = useState([]);
-  const [message, SetMessage] = useState("");
-  const Author = { Men: "men", Bot: "bot" };
+  const [message, SetMessage] = useState('');
+  const Author = { Men: 'men', Bot: 'bot' };
   const handleInput = (ev) => SetMessage(ev.target.value);
 
-  const handleClick = (ev) => {
+  const handleClick = () => {
     SetMessageList([...messageList, { Mess: message, Author: Author.Men }]);
-    SetMessage("");
+    SetMessage('');
   };
 
   const sendMess = () => {
-    SetMessageList([...messageList, { Mess: "Hello", Author: Author.Bot }]);
+    SetMessageList([...messageList, { Mess: 'Hello', Author: Author.Bot }]);
   };
-
 
   useEffect(() => {
     if (messageList.length)
-      if (messageList[messageList.length - 1].Author === Author.Men) 
-        setTimeout(() => sendMess(), 1500); 
+      if (messageList[messageList.length - 1].Author === Author.Men)
+        setTimeout(() => sendMess(), 1500);
   }, [messageList]);
 
   return (
     <>
       <ul>
         {messageList.map((el) => (
-          <li>{el.Author + " : " + el.Mess}</li>
+          <li>{el.Author + ' : ' + el.Mess}</li>
         ))}
       </ul>
       <input type="text" onInput={handleInput} value={message}></input>
